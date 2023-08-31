13aabfe365828dba7f7869cb8d5b26a39a79109a
Vladyslav Prokhasko <vlad56445@gmail.com>
2023-08-31 13:00:37 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import usersFromServer from './api/users';
 import categoriesFromServer from './api/categories';
 import productsFromServer from './api/products';
 
-// const products = productsFromServer.map((product) => {
-//   const category = null; // find by product.categoryId
-//   const user = null; // find by category.ownerId
-
-//   return null;
-// });
+/* eslint-disable */
 
 export const App = () => {
   const [selectedUser, setSelectedUser] = useState(null);
